761b3f40b27e71124e0eaa2a3303485679d1f73e
El Juli <mayorga.julian@gmail.com>
2014-08-25 19:09:10 -0300

    feat(*): scaffold angular app

---
+++ app/scripts/inmobiliaria.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('inmobiliaria', ['ngResource', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
