250f71e2268e3786292f04799e5955189f658c2a
El Juli <mayorga.julian@gmail.com>
2014-08-26 01:04:22 -0300

    fix(protractor): remove conditional capabilities

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,23 +1,13 @@
 // An example configuration file.
-var capabilities;
-if (process.env.TRAVIS === true) {
-  capabilities = {
-    'phantomjs.binary.path':'./node_modules/phantomjs/bin/phantomjs',
-    'browserName': 'phantomjs'
-  };
-} else {
-  capabilities = {
-    'browserName': 'chrome'
-  }
-}
-
 exports.config = {
   // The address of a running selenium server.
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar', // Make use you check the version in the folder
 
   // Capabilities to be passed to the webdriver instance.
-  capabilities: capabilities,
+  capabilities: {
+    'browserName': 'chrome'
+  },
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
