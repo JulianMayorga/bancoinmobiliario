b8b032ee15574557a303bcad23c225310d751ad9
El Juli <mayorga.julian@gmail.com>
2014-08-26 17:34:24 -0300

    feat(unit-tests): make gulp test-watch task to autowatch tests

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -6,26 +6,42 @@ var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep');
 
-gulp.task('test', function() {
-  var bowerDeps = wiredep({
-    directory: 'app/bower_components',
-    exclude: ['bootstrap-sass-official'],
-    dependencies: true,
-    devDependencies: true
-  });
+var bowerDeps = wiredep({
+  directory: 'app/bower_components',
+  exclude: ['bootstrap-sass-official'],
+  dependencies: true,
+  devDependencies: true
+});
 
-  var testFiles = bowerDeps.js.concat([
-    'app/scripts/**/*.js',
-    'test/unit/**/*.js'
-  ]);
+var testFiles = bowerDeps.js.concat([
+  'app/scripts/**/*.js',
+  'test/unit/**/*.js'
+]);
+
+var karmaConfig = 'test/karma.conf.js';
+
+gulp.task('test', function() {
+  return gulp.src(testFiles)
+  .pipe($.karma({
+    configFile: karmaConfig,
+    action: 'run'
+  }))
+  .on('error', function(err) {
+    // Make sure failed tests cause gulp to exit non-zero
+    throw err;
+  });
+});
+
+gulp.task('test-watch', function() {
 
   return gulp.src(testFiles)
-    .pipe($.karma({
-      configFile: 'test/karma.conf.js',
-      action: 'run'
-    }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
+  .pipe($.karma({
+    configFile: karmaConfig,
+    action: 'watch'
+  }))
+  .on('error', function(err) {
+    // Make sure failed tests cause gulp to exit non-zero
+    throw err;
+  });
 });
+
