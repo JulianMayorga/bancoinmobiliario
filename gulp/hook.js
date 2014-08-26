794c3a226f04db236f3de2f44025f72f67a5892b
El Juli <mayorga.julian@gmail.com>
2014-08-26 18:23:21 -0300

    feat(.git): run test tasks before every commit

---
+++ gulp/hook.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var symlink = require('gulp-symlink');
+
+gulp.task('hook', function () {
+  return gulp.src('./.pre-commit')
+  .pipe(symlink('.git/hooks/', 'pre-commit'));
+});
